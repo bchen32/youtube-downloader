b4154bcf0ab9bc34e3ee319fc0d6fd4db8aa7912
StormFalcon32 <benchen9708@gmail.com>
2021-09-01 12:30:21 -0500

    Downloaded some more videos and updated start_ind

--- Downloader.py
+++ Downloader.py
@@ -3,10 +3,10 @@ from time import sleep
 
 playlist = pytube.Playlist('https://www.youtube.com/playlist?list=PLFITtxHy9Gk4gsWyU7uCVHK2Xp6W4pW35')
 ind = 0
-start_ind = 295
+start_ind = 311
 for video in playlist.videos:
-    ind += 1
-    if ind >= start_ind:
-        print('Downloading {} {} / {}'.format(video.title, ind, len(playlist.videos)))
-        video.streams.first().download('C:/Data/Python/Youtube-Downloader/Downloads/')
-        sleep(1)
+	ind += 1
+	if ind >= start_ind:
+		print('Downloading {} {} / {}'.format(video.title, ind, len(playlist.videos)))
+		video.streams.filter(file_extension='mp4').first().download('C:/Data/Python/Youtube-Downloader/Downloads/')
+		sleep(1)
