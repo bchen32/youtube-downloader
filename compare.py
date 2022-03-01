93fb7796daf50337083fd0a70bb2eee191fe4970
StormFalcon32 <benchen9708@gmail.com>
2022-02-28 23:18:19 -0600

    Improve comparison printing

--- compare.py
+++ compare.py
@@ -9,14 +9,21 @@ ind = 0
 files = [x[2] for x in os.walk(directory)]
 files[0] = [file[:-4] for file in files[0]]
 titles = []
+not_found = []
+deleted = []
 for video in playlist.videos:
 	ind += 1
 	title = video.streams.filter(file_extension='mp4').first().default_filename[:-4]
 	titles.append(title)
 	print('Traversing {} {} / {}'.format(title, ind, len(playlist.videos)))
 	if title not in files[0]:
-		print('\n----Not found---- {}\n'.format(video.title))
+		not_found.append(video.title)
 	sleep(1)
 for file in files[0]:
 	if file not in titles:
-		print('\n----Deleted---- {}\n'.format(file))
+		deleted.append(file)
+
+for title in not_found:
+	print('\n----Not found---- {}\n'.format(title))
+for title in deleted:
+	print('\n----Deleted---- {}\n'.format(title))
