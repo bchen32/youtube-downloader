da3d40149dbc14ea79dc4b6812b5ffee502d6159
StormFalcon32 <benchen9708@gmail.com>
2022-02-28 23:18:23 -0600

    Update downloads

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ import downloader
 
 directory = 'C:/Data/Python/youtube-downloader/downloads/'
 playlist = pytube.Playlist('https://www.youtube.com/playlist?list=PLFITtxHy9Gk4gsWyU7uCVHK2Xp6W4pW35')
-start_ind = 651
-downloader.download(playlist, directory, start_ind)
+downloader.download(playlist, directory, 726)
 converter.convert_normalize(directory)
